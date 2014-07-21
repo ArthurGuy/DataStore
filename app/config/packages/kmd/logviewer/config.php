ffa59894235a8b86f3311c347303f9d78204f4d0
Arthur Guy <arthur@assys.net>
2014-07-21 13:46:41 +0100

    Logviewer - display more lines

--- app/config/packages/kmd/logviewer/config.php
+++ app/config/packages/kmd/logviewer/config.php
@@ -9,8 +9,8 @@ return array(
         'delete' => array()
     ),
     'log_dirs'   => array('app' => storage_path().'/logs'),
-    'log_order'  => 'asc', // Change to 'desc' for the latest entries first
-    'per_page'   => 10,
+    'log_order'  => 'desc', // Change to 'desc' for the latest entries first
+    'per_page'   => 25,
     'view'       => 'logviewer::viewer',
     'p_view'     => 'pagination::slider'
 
