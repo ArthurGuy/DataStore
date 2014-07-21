ac44406a6dc153dba67fd81294e7c723db27e1f6
Arthur Guy <arthur@assys.net>
2014-07-21 13:42:48 +0100

    Logfile style changed

--- app/start/global.php
+++ app/start/global.php
@@ -31,7 +31,9 @@ ClassLoader::addDirectories(array(
 |
 */
 
-Log::useFiles(storage_path().'/logs/laravel.log');
+//Log::useFiles(storage_path().'/logs/laravel.log');
+$logFile = 'log-'.php_sapi_name().'.txt';
+Log::useDailyFiles(storage_path().'/logs/'.$logFile);
 
 /*
 |--------------------------------------------------------------------------
