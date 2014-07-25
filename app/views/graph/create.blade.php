28612cb7cf9cc3800d3ea52a7ac05c92d64d88ed
Arthur Guy <arthur@assys.net>
2014-07-25 20:58:34 +0100

    Added validation responses to the graph pages

--- app/views/graph/create.blade.php
+++ app/views/graph/create.blade.php
@@ -2,37 +2,43 @@
 
 {{ Form::open(array('route' => 'graph.store')) }}
 
-    <div class="form-group">
+    <div class="form-group {{ $errors->has('name') ? 'has-error has-feedback' : '' }}">
         {{ Form::label('name', 'Name') }}
         {{ Form::text('name', null, array('class'=>'form-control')) }}
+        {{ $errors->first('name', '<span class="help-block">:message</span>') }}
     </div>
 
 
-    <div class="form-group">
+    <div class="form-group {{ $errors->has('streamId') ? 'has-error has-feedback' : '' }}">
         {{ Form::label('streamId', 'Stream') }}
         {{ Form::select('streamId', [""]+$streamDropdown, null, array('class'=>'form-control')) }}
+        {{ $errors->first('streamId', '<span class="help-block">:message</span>') }}
     </div>
 
-    <div class="form-group">
+    <div class="form-group {{ $errors->has('field') ? 'has-error has-feedback' : '' }}">
         {{ Form::label('field', 'Field') }}
         {{ Form::select('field', [], null, array('class'=>'form-control')) }}
+        {{ $errors->first('field', '<span class="help-block">:message</span>') }}
     </div>
 
-    <div class="form-group">
+    <div class="form-group {{ $errors->has('filter_field') ? 'has-error has-feedback' : '' }}">
         {{ Form::label('filter_field', 'Filter') }}
         <div class="row">
             <div class="col-xs-6">
                 {{ Form::select('filter_field', [], null, array('class'=>'form-control')) }}
+                {{ $errors->first('filter_field', '<span class="help-block">:message</span>') }}
             </div>
             <div class="col-xs-6">
                 {{ Form::text('filter', null, array('class'=>'form-control')) }}
+                {{ $errors->first('filter', '<span class="help-block">:message</span>') }}
             </div>
         </div>
     </div>
 
-    <div class="form-group">
+    <div class="form-group {{ $errors->has('time_period') ? 'has-error has-feedback' : '' }}">
         {{ Form::label('time_period', 'Time Period') }}
         {{ Form::select('time_period', $timePeriods, null, array('class'=>'form-control')) }}
+        {{ $errors->first('time_period', '<span class="help-block">:message</span>') }}
     </div>
 
 
--- app/views/graph/edit.blade.php
+++ app/views/graph/edit.blade.php
@@ -2,37 +2,43 @@
 
 {{ Form::open(array('route' => ['graph.update', $graph['id']], 'method'=>'PUT')) }}
 
-    <div class="form-group">
+    <div class="form-group {{ $errors->has('name') ? 'has-error has-feedback' : '' }}">
         {{ Form::label('name', 'Name') }}
         {{ Form::text('name', $graph['name'], array('class'=>'form-control')) }}
+        {{ $errors->first('name', '<span class="help-block">:message</span>') }}
     </div>
 
 
-    <div class="form-group">
+    <div class="form-group {{ $errors->has('streamId') ? 'has-error has-feedback' : '' }}">
         {{ Form::label('streamId', 'Stream') }}
         {{ Form::select('streamId', [""]+$streamDropdown, $graph['streamId'], array('class'=>'form-control')) }}
+        {{ $errors->first('streamId', '<span class="help-block">:message</span>') }}
     </div>
 
-    <div class="form-group">
+    <div class="form-group {{ $errors->has('field') ? 'has-error has-feedback' : '' }}">
         {{ Form::label('field', 'Field') }}
-        {{ Form::select('field', [], $graph['field'], array('class'=>'form-control')) }}
+        {{ Form::select('field', [], $graph['field'], array('class'=>'form-control', 'data-existing'=>$graph['field'])) }}
+        {{ $errors->first('field', '<span class="help-block">:message</span>') }}
     </div>
 
-    <div class="form-group">
+    <div class="form-group {{ $errors->has('filter_field') ? 'has-error has-feedback' : '' }}">
         {{ Form::label('filter_field', 'Filter') }}
         <div class="row">
             <div class="col-xs-6">
-                {{ Form::select('filter_field', [], $graph['filter_field'], array('class'=>'form-control')) }}
+                {{ Form::select('filter_field', [], $graph['filter_field'], array('class'=>'form-control', 'data-existing'=>$graph['filter_field'])) }}
+                {{ $errors->first('filter_field', '<span class="help-block">:message</span>') }}
             </div>
             <div class="col-xs-6">
                 {{ Form::text('filter', $graph['filter'], array('class'=>'form-control')) }}
+                {{ $errors->first('filter', '<span class="help-block">:message</span>') }}
             </div>
         </div>
     </div>
 
-    <div class="form-group">
+    <div class="form-group {{ $errors->has('time_period') ? 'has-error has-feedback' : '' }}">
         {{ Form::label('time_period', 'Time Period') }}
         {{ Form::select('time_period', $timePeriods, $graph['time_period'], array('class'=>'form-control')) }}
+        {{ $errors->first('time_period', '<span class="help-block">:message</span>') }}
     </div>
 
 
@@ -51,11 +57,17 @@
                     $("#field").empty();
                     $("#filter_field").empty();
                     for (var x in streams[i].fields) {
-                        //console.log(streams[i].fields[x]);
+                        var selected = '';
                         if (streams[i].fields[x].type == 'data') {
-                            $("#field").append($("<option value=\""+streams[i].fields[x].key+"\">"+streams[i].fields[x].name+"</option>"));
+                            if ($("#field").attr('data-existing') == streams[i].fields[x].key) {
+                                selected = 'selected="selected"';
+                            }
+                            $("#field").append($("<option value=\""+streams[i].fields[x].key+"\" "+selected+">"+streams[i].fields[x].name+"</option>"));
                         } else if (streams[i].fields[x].type == 'filter') {
-                            $("#filter_field").append($("<option value=\""+streams[i].fields[x].key+"\">"+streams[i].fields[x].name+"</option>"));
+                            if ($("#filter_field").attr('data-existing') == streams[i].fields[x].key) {
+                                selected = 'selected="selected"';
+                            }
+                            $("#filter_field").append($("<option value=\""+streams[i].fields[x].key+"\" "+selected+">"+streams[i].fields[x].name+"</option>"));
                         }
                     }
 
