873235c81501fd255ff4c03673d76f7b58dce9c7
Arthur Guy <arthur@assys.net>
2015-07-01 23:32:51 +0100

    also set the last movement date on the parent location

--- app/Listeners/UpdateLocationHomeState.php
+++ app/Listeners/UpdateLocationHomeState.php
@@ -29,13 +29,18 @@ class UpdateLocationHomeState implements ShouldQueue
         $parentLocation = Location::find($event->location->parent_id);
         if (($event->location->type == 'room') && $parentLocation) {
             $home = false;
+            $lastMovement = false;
             foreach ($parentLocation->rooms()->get() as $room) {
                 //If any room has activity set the building state to home
                 if ($room->home == 1) {
                     $home = true;
+                    $lastMovement = $room->last_movement;
                 }
             }
             $parentLocation->home = $home;
+            if ($lastMovement) {
+                $parentLocation->last_movement = $lastMovement;
+            }
             $parentLocation->save();
         }
     }
