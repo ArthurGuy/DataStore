0cb1cd28531fae5afee52178b309b7ff80539603
Arthur Guy <arthur@assys.net>
2015-07-03 21:17:29 +0100

    Removed old update method

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -19,7 +19,7 @@ class EventServiceProvider extends ServiceProvider
             UpdateRemoteDeviceState::class
         ],
         'App\Events\LocationHomeStateChanged' => [
-            UpdateLocationHomeState::class
+
         ],
     ];
 
