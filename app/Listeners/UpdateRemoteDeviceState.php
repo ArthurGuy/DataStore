c291668023042b80ce278bdf0c56f7832bd7a362
Arthur Guy <arthur@assys.net>
2015-07-14 10:34:52 +0100

    debugging

--- app/Listeners/UpdateRemoteDeviceState.php
+++ app/Listeners/UpdateRemoteDeviceState.php
@@ -27,6 +27,7 @@ class UpdateRemoteDeviceState implements ShouldQueue
      */
     public function handle(DeviceStateChanged $event)
     {
+        Log::debug("Processing device state change");
         $device = $event->device;
         $client = new \GuzzleHttp\Client();
         if ($device->state_type == 'binary') {
