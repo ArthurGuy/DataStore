9d80d599ae642665aec2f65e3ad7e35ac6a414bf
Arthur Guy <arthur@assys.net>
2015-07-16 16:48:27 +0100

    Service worker update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1600,9 +1600,9 @@ function apiResponse(request) {
         console.log("Service Worker: API Fetch error");
     });
 }
-}).call(this,require("DF1urx"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_c6192eac.js","/")
+}).call(this,require("DF1urx"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_3c3326c4.js","/")
 },{"./../versions":7,"DF1urx":4,"buffer":1,"serviceworker-cache-polyfill":5}],7:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
-module.exports = {'dashboard':'1.0.116'}
+module.exports = {'dashboard':'1.0.118'}
 }).call(this,require("DF1urx"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/../versions.js","/..")
 },{"DF1urx":4,"buffer":1}]},{},[6])
--- resources/assets/js/versions.js
+++ resources/assets/js/versions.js
@@ -1,1 +1,1 @@
-module.exports = {'dashboard':'1.0.117'}
+module.exports = {'dashboard':'1.0.118'}
