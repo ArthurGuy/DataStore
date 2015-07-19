900d70e5300685f5cb0d3040a7a7bba2eaf65c69
Arthur Guy <arthur@assys.net>
2015-07-19 11:55:34 +0100

    more sp testing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use App\Models\Location;
+use App\Models\Test;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Location::all();
+        new Test();
     }
 
     /**
